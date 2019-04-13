12d6dbc15a99080bb0f550bb2c39a69f080f9b6a
Tomasz Martysk <tomek@martynski.pl>
2019-04-13 17:35:53 +0200

    use root ca

--- src/localSSL.ts
+++ src/localSSL.ts
@@ -1,38 +1,52 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import * as sslValidator from "ssl-validator";
-import { exec } from "./utils";
+import { exec, sleep } from "./utils";
 
 export interface ISSLInfo {
   key: string;
   cert: string;
   ca?: string;
+  csr?: string;
 }
 
 export const generateLocalSSL = async (domain: string): Promise<ISSLInfo> => {
-  const dir = path.join(__dirname, "..", ".certificates", domain);
+  const rootDir = path.join(__dirname, "..", ".certificates");
+  const dir = path.join(rootDir, domain);
 
   await fs.ensureDir(dir);
 
   const info: ISSLInfo = {
     key: path.join(dir, `privkey.pem`),
-    cert: path.join(dir, `cert.pem`)
+    cert: path.join(dir, `cert.pem`),
+    csr: path.join(dir, "signing.csr")
   };
 
-  if ((await fs.pathExists(info.key)) && (await fs.pathExists(info.cert))) {
-    const certContent = await fs.readFile(info.cert);
-    const keyContent = await fs.readFile(info.key);
+  const rootCACrt = path.join(rootDir, ".root", "rootCA.crt");
+  const rootCAKey = path.join(rootDir, ".root", "rootCA.key");
 
-    if (await sslValidator.validateCertKeyPair(certContent, keyContent)) {
-      // certificate is ok, no need to renew or anuthing
-      return info;
-    }
-  }
+  await exec(`openssl genrsa -out ${info.key} 2048`);
 
   await exec(
-    `openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ${
-      info.key
-    } -out ${info.cert} -subj '/CN=${domain}'`
+    `openssl req -new -sha256 -key ${info.key} -subj '/CN=${domain}' -out ${
+      info.csr
+    }`
+  );
+
+  const command = `openssl req -new -sha256 -key ${
+    info.key
+  } -subj "/C=US/ST=CA/O=Ez-proxy, Inc./CN=${domain}" -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=DNS:${domain},DNS:*.${domain}")) -out ${
+    info.csr
+  }`;
+
+  await exec(`bash -c '${command}'`);
+
+  await exec(
+    `openssl x509 -req -in ${
+      info.csr
+    } -CA ${rootCACrt} -CAkey ${rootCAKey} -CAcreateserial -out ${
+      info.cert
+    } -days 3650 -sha256`
   );
 
   return info;
